61715ce24a5bece8f435b9436b8be72a57a80f1f
Ramshad <ramshadei@gmail.com>
2023-03-13 13:27:24 +0000

    staging commit

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+// this is example
 Route::get('/', function () {
     return view('welcome');
 });
